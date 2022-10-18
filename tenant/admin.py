68a7edc17751aa2701b567d591a1dcfe5ae0226e
AhmedIbrahim226 <ae4755740@gmail.com>
2022-10-18 23:49:56 +0200

    modify admin

--- tenant/admin.py
+++ tenant/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Tenant, Member
 
 
-admin.site.register(Tenant)
+@admin.register(Tenant)
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subdomain']
+
+
 admin.site.register(Member)
