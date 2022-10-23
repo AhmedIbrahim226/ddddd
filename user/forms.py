fe7c22c4484b78cf2ced4098041d48369a573394
AhmedIbrahim226 <ae4755740@gmail.com>
2022-10-23 23:48:58 +0200

    C4

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth import authenticate, login
 from django.core.exceptions import ValidationError
 
 
